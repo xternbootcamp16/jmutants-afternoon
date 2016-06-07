e26a63d92c3ad9a71b204182e0976abfee25f159
Dave Strus <dave@getfretless.com>
2016-06-07 15:13:31 -0400

    Create mutant - succinct.

--- js/succinct.js
+++ js/succinct.js
@@ -7,3 +7,22 @@ $.get({
     });
   }
 });
+
+// create-mutants.js
+var mutant = {
+  mutant_name: 'Michaellllllll',
+  real_name: 'Miiiiichaaeeel',
+  power: 'Yeah.'
+};
+
+function createMutant(mutant) {
+  $.post({
+    url: url,
+    data: {
+      mutant: mutant
+    },
+    success: function(x) {
+      alert(x);
+    },
+  });
+}
